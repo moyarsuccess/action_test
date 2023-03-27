7d88e9416c45792a63588453d7f693c5a19ef8a2
Moyar <moyar@fireworkhq.com>
2023-03-27 23:36:12 +0000

    Release a new RC

--- app/src/main/java/com/moradyar/githubactions/AppVersion.kt
+++ app/src/main/java/com/moradyar/githubactions/AppVersion.kt
@@ -6,7 +6,7 @@ const val major = 6
 const val minor = 0
 const val patch = 0
 const val isSnapshot = false
-const val isRc = false
+const val isRc = true
 const val rcVersion = 1
 const val versionName = "$major.$minor.$patch"
 val fullVersionName: String
